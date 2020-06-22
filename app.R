9d68a1927014ea8096b6941b4e1c6a80c09b3126
Christine.Stawitz <Christine.Stawitz@nmfs.local>
2020-06-22 15:16:51 -0700

    Test out dynamic inputs

--- app.R
+++ app.R
@@ -1,17 +1,22 @@
 library(shiny)
 
+inputs <-list("nyears","nseasons","minAge","maxAge","recType","growthType","mortType")
 # Define UI for miles per gallon app ----
-ui <- pageWithSidebar(
+ui <- fluidPage(
   
   # App title ----
-  headerPanel("Generate inputs for stock assessment models"),
+  titlePanel("Generate inputs for stock assessment models"),
   
-  # Sidebar panel for inputs ----
-  sidebarPanel(),
+  # Div panel for inputs ----
   
+    par <- div(id="input"),
+    for(i in seq_len(length(inputs))){
+      par <- shiny::tagAppendChild(par, textInput(inputId=paste("input",i,sep=""),inputs[[i]]))
+    },
+    par,
+  actionButton("submit", "Enter", class="btn-primary")
+  )
   # Main panel for displaying outputs ----
-  mainPanel()
-)
 
 # Define server logic to plot various variables against mpg ----
 server <- function(input, output) {
